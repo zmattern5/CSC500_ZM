03f720274d16e0217fb8d642338227e823ce4913
zmattern5 <zachary.mattern@csuglobal.edu>
2024-08-18 20:09:20 -0400

    assignments M1 Critical Thinking

---
+++ CriticalThinkingAssignment1_AddAndSubtract.py
@@ -0,0 +1,4 @@
+user_num = int(input('Enter integer:\n'))
+user_num2 = int(input('Enter second integer:\n'))
+print('Adding the two numbers together results:',user_num+user_num2)
+print('Subtracting the first number by the second results:',user_num-user_num2)
---
+++ CriticalThinkingAssignment1_MultiplyAndDivide.py
@@ -0,0 +1,4 @@
+user_num = int(input('Enter integer:\n'))
+user_num2 = int(input('Enter second integer:\n'))
+print('Multiplying the two numbers together results:',user_num*user_num2)
+print('Dividing the first number by the second results:',user_num/user_num2)
