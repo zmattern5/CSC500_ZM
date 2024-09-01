23171c995252ee2abdf017cbd15ec8d6d8da0261
zmattern5 <zachary.mattern@csuglobal.edu>
2024-09-01 10:49:09 -0400

    Add files via upload

--- Module_3_ZMattern_Timer.py
+++ Module_3_ZMattern_Timer.py
@@ -21,9 +21,9 @@ def alarm_check():
         #If current hours are less then the stopping point then increment
         if int(current_hours) < hours_to_stop:
             current_hours = (int(current_hours) + 1)
-        #If current hours is currently 24 then reset back to 1AM
+        #If current hours is currently 24 then reset back to 0
         else:
-            current_hours = 1
+            current_hours = 0
     #Join hours and minutes
     set_alarm = (str(current_hours)+":"+current_minutes)
     #Clean string
